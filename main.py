2cdf553572ca0ef0f6f52184647beafd19ab5b92
Sharapatov Nikita <neffshar@gmail.com>
2023-03-19 18:03:34 +0300

    logs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import gspread
 import time
+import logging
 
 sa = gspread.service_account(filename='serv_name.json')
 sh = sa.open('Test')
 wks = sh.worksheet('Class Data')
-
+logging.basicConfig(filename='logs.log', level=logging.ERROR)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
@@ -21,20 +22,20 @@ while True:
     r = requests.get(get_url(page), headers=headers)
     antifreeze = 10000
 
+    if r.status_code != 200:
+        logging.error(f'Response code is {r.status_code}')
+        break
     while 'data' in r.json() and antifreeze > 0:
         antifreeze -= 1
         page += 1
-        if r.status_code == 200:
-            json_data = r.json()
-            for obj in json_data['data']:
-                objects.append([
-                    f"{obj['pair']['symbol']}/{obj['pair']['symbolRef']}",
-                    obj['price'],
-                    f"https://www.dextools.io/app/en/ether/pair-explorer/{obj['_id']['pair']}",
-                    obj['_id']['token']
-                ])
-        else:
-            print('Не удалось получить данные. Код ошибки:', r.status_code)
+        json_data = r.json()
+        for obj in json_data['data']:
+            objects.append([
+                f"{obj['pair']['symbol']}/{obj['pair']['symbolRef']}",
+                obj['price'],
+                f"https://www.dextools.io/app/en/ether/pair-explorer/{obj['_id']['pair']}",
+                obj['_id']['token']
+            ])
         r = requests.get(get_url(page), headers=headers)
 
     wks.update('A2', objects)
